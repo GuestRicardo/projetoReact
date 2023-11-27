6d816cef338002a4f9e684d1681a697209773967
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-11-27 14:30:30 -0300

    formatando

---
+++ src/templates/home/teste.jsx
@@ -0,0 +1,18 @@
+
+import { Component } from 'react';
+import './style.css';
+
+export class Home extends Component {
+  state= {
+    counter: 0
+  } 
+
+  render() {
+    return (
+      <div>
+        <h1>Testes</h1>
+        <h2>{this.state.counter}</h2>
+      </div>
+  );
+  }
+}
