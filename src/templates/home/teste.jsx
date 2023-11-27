eb20aba958b2da991519395aafa6289307c7bdad
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-11-27 14:44:30 -0300

    formatando

--- src/templates/home/teste.jsx
+++ src/templates/home/teste.jsx
@@ -3,13 +3,16 @@ import { Component } from 'react';
 import './style.css';
 
 export class Home extends Component {
-  state= {
+  state = {
     counter: 0
-  } 
+  }
 
-  handleClick = ()=>{
-    this.setState({counter: this.state.counter + 1});
-    console.log(this.state.counter);
+  handleClick = () => {
+    this.setState({ counter: this.state.counter + 1 },
+      () => {
+        console.log(this.state.counter);
+      }
+    );
     /**
      * Oq esta sendo manipulado acima, 
      * nao é o DOM do navegador,
@@ -18,7 +21,10 @@ export class Home extends Component {
      * o setState ele compara os 2 DOMS e procura onde esta a diferença
      * é assim q o react funciona, ai por essa comparação ele pega todos os elementos
      * da pagina e comparar e joga o dom mudado e é setado no DOM real, isso é um codigo assicrono
-     * 
+     * codigo assicrono é quando ele esta na espera, equnto o outros codigos estao em simultaneo, ai ele começa com
+     * um estado e é preciso usar o setState q vai sicronizar com um Callback
+     * n o exemplo acima esta sendo mostrado como funciona, this.setState esta sendo assicrono com a função q esta sendo carregado o console.log
+     * entao, handleClick esta passando para o react
      * 
      */
   }
@@ -30,6 +36,6 @@ export class Home extends Component {
         <h2>{this.state.counter}</h2>
         <button onClick={this.handleClick}>Incremente</button>
       </div>
-  );
+    );
   }
 }
