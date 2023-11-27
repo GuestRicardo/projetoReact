74ed9fef2f0a4d22eea4231f46c761e596ea37d4
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-11-27 14:33:41 -0300

    formatando

--- src/templates/home/teste.jsx
+++ src/templates/home/teste.jsx
@@ -7,11 +7,16 @@ export class Home extends Component {
     counter: 0
   } 
 
+  handleClick = ()=>{
+    this.setState({counter: this.state.counter + 1})
+  }
+
   render() {
     return (
       <div className='container'>
         <h1>Testes</h1>
         <h2>{this.state.counter}</h2>
+        <button onClick={this.handleClick}>Incremente</button>
       </div>
   );
   }
