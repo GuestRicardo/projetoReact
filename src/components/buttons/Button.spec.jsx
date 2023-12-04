fcf71082731d0b9d83b1f1340a8a8bbab8ff9f68
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-04 16:39:25 -0300

    formatando

--- src/components/buttons/Button.spec.jsx
+++ src/components/buttons/Button.spec.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import { ButtonCarregar } from "./button-carregar";
 
-
+//para garantir q o botao esta sendo carregado
 describe('<ButtonCarregar />',()=>{
     it('should render the button with the text', ()=>{
         render(<ButtonCarregar text='load more' />);
@@ -11,4 +11,15 @@ describe('<ButtonCarregar />',()=>{
         const button = screen.getByRole('button', { name: /load more/i });
         expect(button).toBeInTheDocument();
     })
-})
+});
+//para garantir q esta sendo chamado as funções
+describe('<ButtonCarregar />',()=>{
+    it('should render the button with the text', ()=>{
+        render(<ButtonCarregar text='load more' />);
+
+        expect.assertions(1);
+
+        const button = screen.getByRole('button', { name: /load more/i });
+        expect(button).toBeInTheDocument();
+    })
+});
