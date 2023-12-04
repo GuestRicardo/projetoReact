457ddabed206253e5edbbcf7e8ce9dc2b3fcf1db
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-04 15:50:47 -0300

    formatando

---
+++ src/components/buttons/Button.spec.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react"
+import { ButtonCarregar } from "./button-carregar";
+
+
+describe('<ButtonCarregar />',()=>{
+    it('should render the button with the text', ()=>{
+        render(<ButtonCarregar />);
+
+        const button = screen.
+    })
+})
