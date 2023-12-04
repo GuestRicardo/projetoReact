9a64c214d56277bd7869f9a5aebea6673eed8b30
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-04 14:42:27 -0300

    formatando

--- src/templates/home/Home.test.jsx
+++ src/templates/home/Home.test.jsx
@@ -1,11 +1,6 @@
 describe('<Home />', ()=>{
-    it('test one',()=>{
+    it('is a dummy test',()=>{
         expect(1).toBe(1);
     });
-    it('test two',()=>{
-        expect(1).toBe(1);
-    });
-    it('test three',()=>{
-        expect(1).toBe(1);
-    });
+
 });
