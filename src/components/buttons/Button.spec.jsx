8d1c038b06b432f46d16651d6818a092d5d1e68d
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-12 14:32:15 -0300

    formatando

--- src/components/buttons/Button.spec.jsx
+++ src/components/buttons/Button.spec.jsx
@@ -2,9 +2,9 @@ import { render, screen } from "@testing-library/react"
 import { ButtonCarregar } from "./button-carregar";
 import userEvent from "@testing-library/user-event";
 
-describe('<ButtonCarregar />',()=>{
+describe('<ButtonCarregar />', () => {
     //para garantir q o botao esta sendo carregado
-    it('should render the button with the text', ()=>{
+    it('should render the button with the text', () => {
         render(<ButtonCarregar text='load more' />);
 
         expect.assertions(1);
@@ -12,9 +12,9 @@ describe('<ButtonCarregar />',()=>{
         const button = screen.getByRole('button', { name: /load more/i });
         expect(button).toBeInTheDocument();
     });
-    
+
     //para garantir q as funções sejam carregadas, click no botao    
-    it('should call function on button click', ()=>{
+    it('should call function on button click', () => {
         const fn = jest.fn();
         render(<ButtonCarregar text='load more' onClick={fn} />);
 
@@ -24,12 +24,16 @@ describe('<ButtonCarregar />',()=>{
         expect(fn).toHaveBeenCalledTimes(1);
     });
 
-    //para testar o disable  esta funcionando 
-    it('should be enable when disable is false', ()=>{      
+    //para testar o disable  esta ativado
+    it('should be enable when disable is true', () => {
+        render(<ButtonCarregar text='load more' disable={true} />);
+        const button = screen.getByRole('button', { name: /load more/i });
+        expect(button).toBeDisabled();
+    });
+    //para testar o disable  esta desativado
+    it('should be enable when disable is false', () => {
         render(<ButtonCarregar text='load more' disable={false} />);
-
-        const button = screen.getByRole('button', { name: /load more/i });     
-
+        const button = screen.getByRole('button', { name: /load more/i });
         expect(button).toBeEnabled();
     });
 })
