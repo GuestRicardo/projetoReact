16dc9f619056cba879b58ed7eb229d5b885bdd26
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-12 16:29:24 -0300

    formatando

--- src/components/PostCard/Post.spec.jsx
+++ src/components/PostCard/Post.spec.jsx
@@ -1,6 +1,9 @@
+import { PostCard } from ".";
+import { render } from "@testing-library/react";
+
 describe('<PostCard />', () => {
     //aqui precisar ser testado se tudo foi renderizado corretamente é somente isso
-    it('', () => {
-
+    it('shoud render PostCard correctly', () => {
+        render(<PostCard />)
     })
 });
