f16a6c67212f065c2b4a8e8237d10a4db815ee17
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-12 16:45:05 -0300

    formatando

--- src/components/PostCard/Post.spec.jsx
+++ src/components/PostCard/Post.spec.jsx
@@ -13,7 +13,8 @@ describe('<PostCard />', () => {
     //aqui precisar ser testado se tudo foi renderizado corretamente é somente isso
     it('shoud render PostCard correctly', () => {
         const { debug } = render(<PostCard {...mock} />);
-        //esse debug é tipo um console log, para saber o q esta na tela
+        //esse debug é tipo um console log, para saber o q esta na tela,
+        // esse tipo de teste e conhecido como snapshot, pois ele tira um print do elemento
         debug();
     });
 });
