86b27e12e6030569a93e34600e820e147943d299
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-13 14:52:43 -0300

    formatando

--- src/components/PostCard/Post.spec.jsx
+++ src/components/PostCard/Post.spec.jsx
@@ -2,7 +2,7 @@ import { PostCard } from ".";
 import { render } from "@testing-library/react";
 
 //dados falsos, para fazer o teste(mock)
-const mock = {
+const props = {
     title: 'title 1',
     body: 'body 1',
     id: '1',
@@ -12,7 +12,7 @@ const mock = {
 describe('<PostCard />', () => {
     //aqui precisar ser testado se tudo foi renderizado corretamente é somente isso
     it('shoud render PostCard correctly', () => {
-        const { debug } = render(<PostCard {...mock} />);
+        const { debug } = render(<PostCard {...props} />);//esse tipo de destruction é chamado de props
         //esse debug é tipo um console log, para saber o q esta na tela,
         // esse tipo de teste e conhecido como snapshot, pois ele tira um print do elemento
         debug();
