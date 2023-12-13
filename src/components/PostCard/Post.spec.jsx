5d4ab9f53e1fb867f7faa883d155a5cb2fd73888
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-13 16:00:22 -0300

    formatando

--- src/components/PostCard/Post.spec.jsx
+++ src/components/PostCard/Post.spec.jsx
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { postCardPropsMock } from "./mock";
 import { toBeInTheDocument } from "@testing-library/jest-dom/matchers";
 
+
 const props = postCardPropsMock;
 
 describe('<PostCard />', () => {
@@ -17,14 +18,22 @@ describe('<PostCard />', () => {
         //ou
         //pode se usar expressao regular, so q é menos criterioso, pq pega somente um trecho especifico
         //expect(screen.getByRole('img', { name: /title 1/i })).toBeInTheDocument();
-        
+
         //aq so esta sendo checado se tem o titulo no documento e imagem prontas
         expect(screen.getByRole('img', { name: 'title 1' }))
-        //entao sera verificado se ela contem o atributo de src, com o caminho da imagem
-        .toHaveAttribute('src', 'img/img.png');
+            //entao sera verificado se ela contem o atributo de src, com o caminho da imagem
+            .toHaveAttribute('src', 'img/img.png');
         //agora sera verificado o heading()
         expect(screen.getByRole('img', { name: /title 1/i })).toBeInTheDocument();
         //agora sera testado o paragrafo
         expect(screen.getByText('body 1')).toBeInTheDocument();
     });
+    //aq esta sendo tirado uma foto do componente q esta sendo executado acima,
+    // para confirma se teve alguma mudança no teste, ele salvara o print literalmente
+    // e estara dentro de uma pasta
+    it('shoud match snapshot', () => {
+        const { container } = render(<PostCard {...props} />);
+        expect(container.firstChild).toMatchInlineSnapshot();
+    }); 
+
 });
