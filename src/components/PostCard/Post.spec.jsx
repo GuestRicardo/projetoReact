bc7483ed9d9ffd7a63e0744051dfa7f54be152b9
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-13 16:06:29 -0300

    formatando

--- src/components/PostCard/Post.spec.jsx
+++ src/components/PostCard/Post.spec.jsx
@@ -9,7 +9,7 @@ const props = postCardPropsMock;
 describe('<PostCard />', () => {
     //aqui precisar ser testado se tudo foi renderizado corretamente é somente isso
     it('shoud render PostCard correctly', () => {
-        render(<PostCard {...props} />);//esse tipo de destruction é chamado de props
+        const{ debug } = render(<PostCard {...props} />);//esse tipo de destruction é chamado de props
         //esse debug é tipo um console log, para saber o q esta na tela,
         // esse tipo de teste e conhecido como snapshot, pois ele tira um print do elemento
         //debug();
