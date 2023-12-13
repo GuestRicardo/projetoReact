08f32d1200e5879f181a0d3fcf71d962318e3c19
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-13 15:14:51 -0300

    formatando

--- src/components/PostCard/Post.spec.jsx
+++ src/components/PostCard/Post.spec.jsx
@@ -1,6 +1,7 @@
 import { PostCard } from ".";
 import { render, screen } from "@testing-library/react";
 import { postCardPropsMock } from "./mock";
+import { toBeInTheDocument } from "@testing-library/jest-dom/matchers";
 
 const props = postCardPropsMock;
 
@@ -10,7 +11,8 @@ describe('<PostCard />', () => {
         const { debug } = render(<PostCard {...props} />);//esse tipo de destruction é chamado de props
         //esse debug é tipo um console log, para saber o q esta na tela,
         // esse tipo de teste e conhecido como snapshot, pois ele tira um print do elemento
-        debug();
-        //aq esta falando q tem q ter uma imagem (.img)
+        //debug();
+        //aq esta falando q tem q ter certeza que tem uma imagem no documento e tem q ser (.img)
+        expect(screen.getByRole('img', { name: 'title 1' })).toBeInTheDocument();
     });
 });
