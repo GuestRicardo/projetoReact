972f51f9e4211eb3ac78a5afb0385cabeb713022
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-15 15:24:45 -0300

    formatando

--- src/components/Posts/Posts.spec.jsx
+++ src/components/Posts/Posts.spec.jsx
@@ -1,8 +1,35 @@
-import { render } from "@testing-library/react";
 import { Posts } from '.'; 
+import { render } from "@testing-library/react";
+
+//mock para simular teste
+const props ={
+posts: [
+    {
+        id:1, 
+        title:'titulo 1', 
+        body:'Corpo 1', 
+        cover: 'img/img1.png',
+    },
+    {
+        id:2, 
+        title:'titulo 2', 
+        body:'Corpo 2', 
+        cover: 'img/img2.png',
+    },
+    {
+        id:3, 
+        title:'titulo 3', 
+        body:'Corpo 3', 
+        cover: 'img/img3.png',
+    },
+]
+};
+//fim do mock de teste
 
 describe('<Posts />', ()=>{
    it('should render posts', ()=>{
-    render('<Posts />')
-   }) 
+    const {debug} = render('<Posts />')
+    //debug();
+   });
+
 });
