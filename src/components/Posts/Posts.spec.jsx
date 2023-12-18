7765c3da188139280426904295cbc756fca6032a
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-18 14:40:09 -0300

    formatando

--- src/components/Posts/Posts.spec.jsx
+++ src/components/Posts/Posts.spec.jsx
@@ -1,36 +1,37 @@
-import { Posts } from '.'; 
-import { render } from "@testing-library/react";
+import { Posts } from '.';
+import { render, screen } from "@testing-library/react";
 
 //mock para simular teste
-const props ={
-posts: [
-    {
-        id:1, 
-        title:'titulo 1', 
-        body:'Corpo 1', 
-        cover: 'img/img1.png',
-    },
-    {
-        id:2, 
-        title:'titulo 2', 
-        body:'Corpo 2', 
-        cover: 'img/img2.png',
-    },
-    {
-        id:3, 
-        title:'titulo 3', 
-        body:'Corpo 3', 
-        cover: 'img/img3.png',
-    },
-]
+const props = {
+    posts: [
+        {
+            id: 1,
+            title: 'titulo 1',
+            body: 'Corpo 1',
+            cover: 'img/img1.png',
+        },
+        {
+            id: 2,
+            title: 'titulo 2',
+            body: 'Corpo 2',
+            cover: 'img/img2.png',
+        },
+        {
+            id: 3,
+            title: 'titulo 3',
+            body: 'Corpo 3',
+            cover: 'img/img3.png',
+        },
+    ]
 };
 //fim do mock de teste
-describe('<Posts />', ()=>{
-   it('should render posts', ()=>{
-    const {debug} = render('<Posts />')
-    //debug();
+describe('<Posts />', () => {
+    it('should render posts', () => {
+        const { debug } = render('<Posts {...props} />')
+        //debug();
+        expect(screen.getAllByRole('heading', {name: /title/i}))
+        .toHaveLength(3); //precisa de 3 titulos no post
 
-    
-   });
+    });
 
 });
