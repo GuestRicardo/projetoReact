c2ee56940b72ddaa7784c71060b0e3d635530283
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-20 15:01:16 -0300

    formatando

--- src/components/input/Input.spec.jsx
+++ src/components/input/Input.spec.jsx
@@ -2,12 +2,14 @@ import { screen, render } from "@testing-library/react";
 import { Input } from './index'; 
 
 describe('<Input />', () => {
-    it('should render posts', () => {
-        
-        render(<Input { } />);
+    //ha cada tecla pressionada sera chamada a função handleChange 
+    it('should Call handleChange function on each key pressed', () => {   
+        //esta função nao serve para nada, ela esta aq somente para ser chamada como teste     
+        const fn = jest.fn(); 
+        render(<Input />);
         
     });
-    it('should Call handleChange function on each key pressed', () => {
+    it('', () => {
         const { container } = render(<input {...props} />);
         expect(container.firstChild).toMatchSnapshot();
     });
