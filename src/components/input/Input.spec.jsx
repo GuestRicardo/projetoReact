dc5a8eedaf82f9e29e0a4b6a73ae4f14c0227c26
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2023-12-20 16:24:49 -0300

    formatando

--- src/components/input/Input.spec.jsx
+++ src/components/input/Input.spec.jsx
@@ -1,19 +1,35 @@
 import { screen, render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";  
+import userEvent from "@testing-library/user-event";
 
 import { InputSearch } from '.';
 
 describe('<InputSearch />', () => {
     //ha cada tecla pressionada sera chamada a função handleChange 
-    it('should Call handleChange function on each key pressed', () => {
+    it('should have a value of searchValue', () => {
         //esta função nao serve para nada, ela esta aq somente para ser chamada como teste     
         const fn = jest.fn();
-        render(<InputSearch handleChange={fn} searchValue={'testando'}/>);
+        render(<InputSearch handleChange={fn} searchValue={'testando'} />);
 
+        const input = screen.getByPlaceholderText(/type your search/i);
+        expect(input.value).toBe('testando');
     });
-    // it('', () => {
-    //     const { container } = render(<input {...props} />);
-    //     expect(container.firstChild).toMatchSnapshot();
-    // });
+
+    it('should Call handleChange function on each key pressed', () => {       
+        const fn = jest.fn();
+        render(<InputSearch handleChange={fn} />);
+
+        const input = screen.getByPlaceholderText(/type your search/i);
+        const value = 'o valor';
+
+        userEvent.type(input, value);
+
+        expect(input.value).toBe(value);
+        expect(fn).toHaveBeenCalledTimes(value.length);
+    });
+    it('should match snapshot ', () => {
+        const fn = jest.fn();
+        const { container } = render(<input {...props} />);
+        expect(container.firstChild).toMatchSnapshot();
+    });
 
 });
