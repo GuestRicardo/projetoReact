8692f68ee5724f50c97e4e90c8ac60f967d6e982
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-01-05 16:40:32 -0300

    formatando

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, //ponto e virgula no fim
+  trailingComma: 'all', //medida de uma virgula sobrando
+  singleQuote: true, //
+  printWidth: 120, //largura maxima para quebrar linha
+  tabWidth: 2 // quando clicar em tab fara 2 espaços
+}
