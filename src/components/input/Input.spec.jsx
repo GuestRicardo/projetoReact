73382f8e49bb02f6fca6948e2c4ee43c63df77ac
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-01-08 14:31:39 -0300

    formatando

--- src/components/input/Input.spec.jsx
+++ src/components/input/Input.spec.jsx
@@ -3,18 +3,21 @@ import userEvent from "@testing-library/user-event";
 
 import { InputSearch } from '.';
 
+// eslint-disable-next-line no-undef
 describe('<InputSearch />', () => {
-    //ha cada tecla pressionada sera chamada a função handleChange 
+    //ha cada tecla pressionada sera chamada a função handleChange
     it('should have a value of searchValue', () => {
-        //esta função nao serve para nada, ela esta aq somente para ser chamada como teste     
+        //esta função nao serve para nada, ela esta aq somente para ser chamada como teste
+        // eslint-disable-next-line no-undef
         const fn = jest.fn();
+        // eslint-disable-next-line react/react-in-jsx-scope
         render(<InputSearch handleChange={fn} searchValue={'testando'} />);
 
         const input = screen.getByPlaceholderText(/type your search/i);
         expect(input.value).toBe('testando');
     });
 
-    it('should Call handleChange function on each key pressed', () => {       
+    it('should Call handleChange function on each key pressed', () => {
         const fn = jest.fn();
         render(<InputSearch handleChange={fn} />);
 
@@ -29,7 +32,7 @@ describe('<InputSearch />', () => {
     it('should match snapshot', () => {
         const fn = jest.fn();
         const {container} = render(<InputSearch handleChange={fn} />);
-        
+
         expect(container).toMatchSnapshot();
     });
 
