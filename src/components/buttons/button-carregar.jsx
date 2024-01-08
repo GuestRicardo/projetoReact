70d25a46fbfee7792157d53c03e91375fb054e16
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-01-08 15:35:13 -0300

    formatando

--- src/components/buttons/button-carregar.jsx
+++ src/components/buttons/button-carregar.jsx
@@ -1,19 +1,27 @@
-import { Component } from 'react'
-import './style.css'
+/* eslint-disable react/react-in-jsx-scope */
+import P from 'prop-types'
+import { Component } from 'react';
+import './style.css';
 
 export class ButtonCarregar extends Component {
-    render() {
-        const { text, onClick, disabled } = this.props;
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const { text, onClick, disabled = false } = this.props;
 
-        return (
-            <div className='buttonCenter'>
-                <button
-                    className='buttonC'
-                    onClick={onClick}
-                    disabled={disabled}>
-                    {text}
-                </button>
-            </div>
-        )
-    }
+    return (
+      // eslint-disable-next-line react/react-in-jsx-scope
+      <div className='buttonCenter'>
+        <button className='buttonC' onClick={onClick} disabled={disabled}>{text} </button>
+      </div>
+    )
+  }
+}
+ButtonCarregar.defaultProps = {
+  disabled: false
+};
+
+ButtonCarregar.propTypes = {
+  text: P.string.isRequired,
+  onclick: P.func.isRequired,
+  disabled: P.boll,
 }
